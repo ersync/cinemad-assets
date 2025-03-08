4f1419be4399c2891510d696e15dd03e99755247
Emad Rahimi <121079771+ersync@users.noreply.github.com>
2025-03-08 11:56:21 +0330

    feat(assets): add initial media folder structure

---
+++ cast/avatar/person_seeds.rb
@@ -0,0 +1,23 @@
+# Person seeds for "Avatar" generated from TMDB API
+# Generated on Thu  6 Mar 2025 07:59:07 +0330
+
+person_8691 = Person.create!(name: "Zoe Saldaña", profession: "actor", gender: "female", date_of_birth: "1978-06-19", place_of_birth: "Passaic, New Jersey, USA", credits: 84, biography: "", image_filename: "zoe_saldaña.jpg")
+
+person_10205 = Person.create!(name: "Sigourney Weaver", profession: "actor", gender: "female", date_of_birth: "1949-10-08", place_of_birth: "Manhattan, New York City, New York, USA", credits: 129, biography: "", image_filename: "sigourney_weaver.jpg")
+
+person_17647 = Person.create!(name: "Michelle Rodriguez", profession: "actor", gender: "female", date_of_birth: "1978-07-12", place_of_birth: "Bexar County, Texas, USA", credits: 53, biography: "", image_filename: "michelle_rodriguez.jpg")
+
+person_65731 = Person.create!(name: "Sam Worthington", profession: "actor", gender: "male", date_of_birth: "1976-08-02", place_of_birth: "Surrey, England, UK ", credits: 62, biography: "", image_filename: "sam_worthington.jpg")
+
+person_1771 = Person.create!(name: "Giovanni Ribisi", profession: "actor", gender: "male", date_of_birth: "1974-12-17", place_of_birth: "Los Angeles, California, USA", credits: 69, biography: "", image_filename: "giovanni_ribisi.jpg")
+
+person_32747 = Person.create!(name: "Stephen Lang", profession: "actor", gender: "male", date_of_birth: "1952-07-11", place_of_birth: "New York, New York, U.S", credits: 116, biography: "", image_filename: "stephen_lang.jpg")
+
+person_10964 = Person.create!(name: "Laz Alonso", profession: "actor", gender: "male", date_of_birth: "1974-03-25", place_of_birth: "Washington, District of Columbia, USA", credits: 36, biography: "", image_filename: "laz_alonso.jpg")
+
+person_68278 = Person.create!(name: "Peter Mensah", profession: "actor", gender: "male", date_of_birth: "1959-08-27", place_of_birth: "Chiraa, Ghana", credits: 25, biography: "", image_filename: "peter_mensah.jpg")
+
+person_30485 = Person.create!(name: "CCH Pounder", profession: "actor", gender: "female", date_of_birth: "1952-12-25", place_of_birth: "Georgetown, British Guiana [now Guyana]", credits: 71, biography: "", image_filename: "cch_pounder.jpg")
+
+person_83105 = Person.create!(name: "Debra Wilson", profession: "actor", gender: "female", date_of_birth: "1962-04-26", place_of_birth: "New York City, New York, USA", credits: 76, biography: "", image_filename: "debra_wilson.jpg")
+
